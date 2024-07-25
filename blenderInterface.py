c015fd585fabe78a117f3418bf27114cd7acbd92
Nicholas Karantakis <94494467+Nick2858@users.noreply.github.com>
2024-07-25 10:23:03 -0400

    Update blenderInterface.py
    added print statements to monitor progress in system console while blender runs script

--- blenderInterface.py
+++ blenderInterface.py
@@ -9,7 +9,6 @@ class Branch:
     """
     This class is used to build the branch objects in blender. Branches are made from cylinders and spheres.
     """
-
     def __init__(self, rowData):
         """
         (object, [float,float,...]) -> Branch object
@@ -33,62 +32,65 @@ class Branch:
         self.xyz1 = rowData[8:11]
         self.xyz2 = rowData[11:14]
 
-        # location is center of the cylinder, thus we calculate the average of the previous and successive node coords
+        #location is center of the cylinder, thus we calculate the average of the previous and successive node coords
         self.location = [(self.xyz1[0] + self.xyz2[0]) / 2, (self.xyz1[1] + self.xyz2[1]) / 2,
                          (self.xyz1[2] + self.xyz2[2]) / 2]
 
-        # vector calculated from predID - succID nodes and converted to mathutils.Vector object for functionality
+
+        #vector calculated from predID - succID nodes and converted to mathutils.Vector object for functionality
         self.vector = mathutils.Vector(
             [(self.xyz2[0] - self.xyz1[0]), (self.xyz2[1] - self.xyz1[1]),
              (self.xyz2[2] - self.xyz1[2])])
 
-        # calculate radius of cylinder from volume and length
+        #calculate radius of cylinder from volume and length
         self.radius = math.sqrt(self.volume / (self.length * math.pi))
 
     def make_branch(self):
-        # create a cylinder object in blender using initialized parameters
+
+        #create a cylinder object in blender using initialized parameters
         bpy.ops.mesh.primitive_cylinder_add(enter_editmode=False, align='WORLD', location=(self.location),
                                             radius=self.radius, depth=self.length)
 
-        # assign cylinder object to Branch object and name it based on it's branch (b) id.
+        #assign cylinder object to Branch object and name it based on it's branch (b) id.
         self.cylinder = bpy.context.active_object
         self.cylinder.name = f"b:{self.id}"
 
-        # set rotation of cylinder using mathutils vector functionality to convert vec to angle
+        #set rotation of cylinder using mathutils vector functionality to convert vec to angle
         up_axis = mathutils.Vector([0.0, 0.0, 1.0])
         angle = self.vector.angle(up_axis, 0)
         axis = up_axis.cross(self.vector)
         self.euler = mathutils.Matrix.Rotation(angle, 4, axis).to_euler()
         self.cylinder.rotation_euler = self.euler
 
-        # create sphere object in blender using initialized parameters
-        # each branch is the parent to the sphere/node at it's successive node location
+        #create sphere object in blender using initialized parameters
+        #each branch is the parent to the sphere/node at it's successive node location
         bpy.ops.mesh.primitive_uv_sphere_add(radius=self.radius, enter_editmode=False, align='WORLD',
                                              location=(self.xyz2), scale=(1, 1, 1))
 
-        # assign sphere object to Branch object and name it based on it's node (s - for sphere) id.
+        #assign sphere object to Branch object and name it based on it's node (s - for sphere) id.
         self.sphere = bpy.context.active_object
         self.sphere.name = f"s:{self.succID}"
+
 
     def hollow(self):
         """
         This function is used to hollow out the branch object into tubes to use in CFD.
         """
 
-        # Build replica cylinder with smaller radius assigned to self.hollow_branch
+        #Build replica cylinder with smaller radius assigned to self.hollow_branch
         bpy.ops.mesh.primitive_cylinder_add(enter_editmode=False, align='WORLD', location=(self.location),
                                             radius=self.radius * 0.99, depth=self.length * 2, rotation=self.euler)
         self.hollow_branch = bpy.context.active_object
 
-        # carve the hollow_branch object out of the cylinder using the carver function
+        #carve the hollow_branch object out of the cylinder using the carver function
         carver(self.cylinder, self.hollow_branch)
 
-        # Build replica sphere with smaller radius assigned to self.hollow_sphere
+        #Build replica sphere with smaller radius assigned to self.hollow_sphere
         bpy.ops.mesh.primitive_uv_sphere_add(radius=self.radius * 0.99, enter_editmode=False, align='WORLD',
                                              location=(self.xyz2), scale=(1, 1, 1))
         self.hollow_sphere = bpy.context.active_object
 
-        # carve the hollow_sphere object out of the sphere using the carver function
+        #carve the hollow_sphere object out of the sphere using the carver function
         carver(self.sphere, self.hollow_sphere)
 
     def finalize(self):
@@ -98,8 +100,8 @@ class Branch:
         Then it renames the branch to include information about the branch id, generation and lob numbers.
         """
 
-        # Unite cylinder with sphere using boolean union
-        # must use exact solver and use_self solver option to properly unite objects.
+        #Unite cylinder with sphere using boolean union
+        #must use exact solver and use_self solver option to properly unite objects.
         bpy.context.view_layer.objects.active = self.cylinder
         bool_one = self.cylinder.modifiers.new(type="BOOLEAN", name="bool 1")
         bool_one.object = self.sphere
@@ -107,15 +109,15 @@ class Branch:
         bool_one.solver = "EXACT"
         bool_one.use_self = True
 
-        # Convert mesh to mesh to apply modifiers (this is a little hack to apply modifiers)
+        #Convert mesh to mesh to apply modifiers (this is a little hack to apply modifiers)
         bpy.ops.object.select_all(action="DESELECT")
         self.cylinder.select_set(True)
         bpy.ops.object.convert(target='MESH', keep_original=False)
 
-        # Rename the branch object (saved to self.cylinder) to include branch id, generation and lob info.
+        #Rename the branch object (saved to self.cylinder) to include branch id, generation and lob info.
         self.cylinder.name = f"Branch:{self.id} Gen:{self.generation} Lobe:{self.lobe}"
 
-        # Use deleter function to delete sphere object which is duplicated after union.
+        #Use deleter function to delete sphere object which is duplicated after union.
         deleter(self.sphere)
 
 
@@ -145,7 +147,7 @@ def carver(obj1, obj2, delete=True):
     obj1.select_set(True)
     bpy.ops.object.convert(target='MESH', keep_original=False)
 
-    # delete object 2 unless delete = False is passed.
+    #delete object 2 unless delete = False is passed.
     if delete:
         deleter(obj2)
 
@@ -157,7 +159,7 @@ def deleter(obj):
     This function is used to delete the object that is passed to it in blender.
     """
 
-    # Deselect all objects, then select the object you wish to delete, delete it and then deselect all objects.
+    #Deselect all objects, then select the object you wish to delete, delete it and then deselect all objects.
     bpy.ops.object.select_all(action='DESELECT')
     obj.select_set(True)
     bpy.ops.object.delete()
@@ -169,107 +171,119 @@ def clean():
     This function iterates through a dictionary of nodes that have edge ID values of their connecting edges assigned to
     them. It goes through all the objects that share a node and removes all overlap.
     """
-
-    # Iterate over each node (represented as sphere) which contains list of branches connected to it.
+    
+    
+    print("Cleaning Branches")
+    
+    defSize = len(touchingBranches)
+    iter = 0
+    #Iterate over each node (represented as sphere) which contains list of branches connected to it.
     for sphere, branches in touchingBranches.items():
-
-        # rename sphere and branches to their object names in blender.
+        iter += 1
+        print(f"Iteration:{iter}/{defSize}")
+        #rename sphere and branches to their object names in blender.
         sphere = f"s:{sphere}"
         for branch in range(len(branches)):
             branches[branch] = f"b:{branches[branch]}"
 
-        # append sphere to branches list so all objects at the node are included in the list.
+        #append sphere to branches list so all objects at the node are included in the list.
         branches.append(sphere)
         branches = branches[::-1]
 
-        # Nested for loop to find the object1 for get carved and object 2 for carving with.
+        #Nested for loop to find the object1 for get carved and object 2 for carving with.
         for name1 in branches:
             for name2 in branches:
 
-                # make sure that objects different for carving.
+                #make sure that objects different for carving.
                 if name2 != name1:
 
-                    # Find object 2 in blender object data.
+                    #Find object 2 in blender object data.
                     for obj in bpy.data.objects:
                         if obj.name == name2:
 
-                            # check if object is a sphere
+                            #check if object is a sphere
                             if "s" in name2:
 
-                                # get sphere data from it's parent branch using self.sphere.name
+                                #get sphere data from it's parent branch using self.sphere.name
                                 for b in branchList:
                                     if b.sphere.name == name2:
                                         shape = b
                                         break
 
-                                # build replica sphere using sphere parameters from (shape) object clone
+                                #build replica sphere using sphere parameters from (shape) object clone
                                 bpy.ops.mesh.primitive_uv_sphere_add(radius=shape.radius, enter_editmode=False,
                                                                      align='WORLD',
                                                                      location=(shape.xyz2), scale=(1, 1, 1))
 
-                            # otherwise object is a cylinder
+                            #otherwise object is a cylinder
                             else:
                                 for b in branchList:
                                     if b.cylinder.name == name2:
                                         shape = b
                                         break
 
-                                # build replica cylinder using cylinder parameters from (shape) object clone.
+                                #build replica cylinder using cylinder parameters from (shape) object clone.
                                 bpy.ops.mesh.primitive_cylinder_add(enter_editmode=False, align='WORLD',
                                                                     location=(shape.location),
                                                                     radius=shape.radius, depth=shape.length,
                                                                     rotation=shape.euler)
-                            # asign replica object to obj2
+                            #asign replica object to obj2
                             obj2 = bpy.context.active_object
                             break
 
-                    # get object 1
+                    #get object 1
                     for obj in bpy.data.objects:
                         if obj.name == name1:
                             obj1 = obj
                             obj1.select_set(True)
 
-                    # carve object 1 using object 2 (replica object)
+                    #carve object 1 using object 2 (replica object)
                     carver(obj1, obj2)
 
 
-# --------------------IMPORTANT----------------------
+#--------------------IMPORTANT----------------------
 #
-# set maximum generation number to create in blender
+#set maximum generation number to create in blender
 #
-# ---------------------------------------------------
+#---------------------------------------------------
 
-max_gen = 3
+max_gen = 2
 
-# --------------------IMPORTANT----------------------
+
+#--------------------IMPORTANT----------------------
 #
-# set csv file path to extract branch network data
+#set csv file path to extract branch network data
 #
-# ---------------------------------------------------
+#---------------------------------------------------
 
-path = "TreeData.csv"
+path = "C:\\Users\\nichk\\3DObjConverter\.venv\TreeData2.csv"
 
-# initialize variables for storing list of branch objects (branchList),
-# and dictionary of objects at nodes (touchingBranches)
+#initialize variables for storing list of branch objects (branchList),
+#and dictionary of objects at nodes (touchingBranches)
 branchList = []
 touchingBranches = {}
 
-# read the csv file
+
+print("Reading csv file")
+
+
+#read the csv file
 with open(path, "r", newline='') as data:
     info = csv.reader(data)
     header = next(info)
 
-    # each row in (info) list stores data for each edge
+    print("Making branches")
+    #each row in (info) list stores data for each edge
     for row in info:
         rowData = []
-
-        # convert row data to float type
+        
+        #convert row data to float type
         for elm in row:
             rowData.append(float(elm))
-
-        # check if branch is in generation range
+            
+        #check if branch is in generation range
         if rowData[3] <= max_gen:
-            # build touchingBranch dictionary, ignoring first node which is the inflow.
+            #build touchingBranch dictionary, ignoring first node which is the inflow.
             if rowData[1] != 0:
                 if rowData[1] not in touchingBranches:
                     touchingBranches[rowData[1]] = [rowData[0], ]
@@ -280,20 +294,27 @@ with open(path, "r", newline='') as data:
             else:
                 touchingBranches[rowData[2]].append(rowData[0])
 
-            # initialize branch object using row data (data for one edge)
+            #initialize branch object using row data (data for one edge)
             b = Branch(rowData)
 
-            # build a branch object in blender
+            #build a branch object in blender
             b.make_branch()
 
-            # append branch object to branch list for use later.
+            #append branch object to branch list for use later.
             branchList.append(b)
 
-# Use clean function to remove overlap in branch objects
+print("Cleaning branches")
+#Use clean function to remove overlap in branch objects
 clean()
 
-# Hollow out each branch (cylinder and sphere) and unite the cylinder and sphere, rename branch to include
-# branch info in name (branch id, generation number and lobe number)
+#Hollow out each branch (cylinder and sphere) and unite the cylinder and sphere, rename branch to include
+#branch info in name (branch id, generation number and lobe number)
+
+print("Finalizing Branches")
+branchNum = len(branchList)
+branchIter = 1
 for branch in branchList:
+    print(f"Branch:{branchIter}/{branchNum}")
     branch.hollow()
     branch.finalize()
+    branchIter += 1
