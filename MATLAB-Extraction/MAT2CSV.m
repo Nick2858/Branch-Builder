3977cff0d7f498ecccf183c1a91ee478f26196cb
Nicholas Karantakis <94494467+Nick2858@users.noreply.github.com>
2024-08-15 15:10:30 -0400

    Update and rename defined2CSV.m to MAT2CSV.m

--- MATLAB-Extraction/MAT2CSV.m
+++ MATLAB-Extraction/MAT2CSV.m
@@ -1,14 +1,21 @@
-diameters = cell2mat(net(:,3))./2;
+%Get radius values from net cell by dividing diameter values by 2
+radii = cell2mat(net(:,3))./2;
+
+%Initialize table
 table = [];
-max = 0;
+
+%Create table with diameter and start and end points for each branch
 for i = 1:size(net, 1)
     coordinates = net(i,1);
     coordinates1 = coordinates{:}(1);    
     coordinates2 = coordinates{:}(end);
-    diameter = diameters(i);
-    %table(i,:) = [diameter, getCoordinates(coordinates1, sizIm), getCoordinates(coordinates2, sizIm)]; 
-    table(i,:) = [diameter, coordinates1, coordinates2]; 
+    radius = radii(i); 
+    table(i,:) = [radius, coordinates1, coordinates2]; 
 end
+
+%Go through each branch's junctions list and change their end coordinates
+%to ensure that they share the same end coordinates as sometimes the
+%coordinates are 1 away but not equal. They must be equal.
 
 for i = 1:size(net,1)
     juncs = cell2mat(net(i,11));
@@ -46,15 +53,20 @@ for i = 1:size(net,1)
     
 end
 
+%Create the final table by converting the coordinate values to cartesian
+%coordinates.
 for i = 1:size(table,1)
-    diameter = diameters(i);
-    final_table(i,:) = [diameter, getCoordinates(table(i,2), sizIm), getCoordinates(table(i,3), sizIm)]; 
+    radius = radii(i);
+    final_table(i,:) = [radius, getCoordinates(table(i,2), sizIm), getCoordinates(table(i,3), sizIm)]; 
     
 end
 
-csvwrite(filename,final_table);
+name = append(filename,'.csv');
 
+writematrix(final_table,name);
 
+%Function that converts a 1D coordinate into 3D cartesian coordinates using
+%the image size.
 function XYZ = getCoordinates(multcoords, sizIm)
 
     frame = sizIm(1) * sizIm (2);
